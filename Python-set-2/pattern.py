0ec3069d27af90e406207764e0a7f025bb3af925
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-08-05 08:32:10 +0530

    set-2 partially

---
+++ Python-set-2/pattern.py
@@ -0,0 +1,16 @@
+# Write a program to print the following number pattern using a loop.
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+
+
+for i in range(1, 6):
+    bag = " "
+    for j in range(1,i+1):
+        bag = bag + str(j) + " "
+    print(bag)    
+
+#spacing of print is very important
